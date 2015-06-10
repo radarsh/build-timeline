501e4d68ff99048ce7250f69438c53f7740791a5
radarsh <adarsh.ramamurthy@gmail.com>
2015-06-10 16:01:48 +0100

    Attempt at adding a job list drop down

--- src/main/groovy/com/adarshr/buildtimeline/BuildTimelineView.groovy
+++ src/main/groovy/com/adarshr/buildtimeline/BuildTimelineView.groovy
@@ -1,13 +1,18 @@
 package com.adarshr.buildtimeline
 
 import hudson.Extension
+import hudson.model.AbstractProject
 import hudson.model.Item
 import hudson.model.TopLevelItem
 import hudson.model.View
 import hudson.model.ViewDescriptor
+import hudson.util.ListBoxModel
+import jenkins.model.Jenkins
 import org.kohsuke.stapler.DataBoundConstructor
 import org.kohsuke.stapler.StaplerRequest
 import org.kohsuke.stapler.StaplerResponse
+
+import static hudson.util.ListBoxModel.Option
 
 class BuildTimelineView extends View {
 
@@ -42,5 +47,11 @@ class BuildTimelineView extends View {
     @Extension
     static final class Descriptor extends ViewDescriptor {
         String displayName = 'Build Timeline View'
+
+        ListBoxModel doFillStartJobItems() {
+            Jenkins.instance.getAllItems(AbstractProject).collect {
+                new Option(it.fullDisplayName, it.fullDisplayName)
+            }
+        }
     }
 }
