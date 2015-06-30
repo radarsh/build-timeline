0df0390d76e3cc5d139fd1c1cc173bff559f772e
radarsh <adarsh.ramamurthy@gmail.com>
2015-06-30 17:19:08 +0100

    Backing up half done work

--- src/main/webapp/js/timeline.js
+++ src/main/webapp/js/timeline.js
@@ -1,3 +1,29 @@
+(function (scope) {
+    "use strict";
+
+    var initialised = false;
+
+    scope.Timeline = function (options) {
+        this.options = options;
+    };
+
+    scope.Timeline.prototype = {
+        _setupAutoRefresh: function () {
+            if (!initialised) {
+                setInterval(function(){
+                    this.draw();
+                }.bind(this), 10000);
+                initialised = true;
+            }
+        },
+
+        draw: function () {
+            this._setupAutoRefresh();
+        }
+    };
+})(window);
+
+
 function Timeline(handle) {
     "use strict";
 
@@ -17,103 +43,99 @@ function Timeline(handle) {
         this.setupAutoRefresh();
 
         this.handle.getTimelineData(function(response) {
-            this.doDraw(JSON.parse(response.responseJSON));
+            var painter = new Painter(JSON.parse(response.responseJSON), 'timeline');
+            painter.drawChart();
         }.bind(this));
     };
 
-    this.doDraw = function(data) {
-        console.log(data);
-
-        var timeline = document.getElementById('timeline');
-        timeline.innerHTML = '';
-
-        var min = d3.min(data, function(d) {
-            return d.start;
-        });
-
-        var max = d3.max(data, function(d) {
-            return d.end;
-        });
-
-        var width = timeline.clientWidth;
-
-        var colours = ["red", "purple", "green", "teal", "pink", "yellow", "orange", "blue", "maroon"];
-        var scaleX = d3.time.scale().domain([new Date(min), new Date(max)]).range([0, width]);
-        var xAxis = d3.svg.axis().scale(scaleX).ticks(20);
-
-        var svg = d3
-            .select("#timeline")
+    function Painter(data, containerId) {
+        this.data = data;
+        this.container = document.getElementById(containerId);
+        this.width = this.container.clientWidth;
+        this.min = d3.min(this.data, function(d) {return d.start;});
+        this.max = d3.max(this.data, function(d) {return d.end;});
+        this.colours = ["red", "purple", "green", "teal", "pink", "yellow", "orange", "blue", "maroon"];
+        this.scaleX = d3.time.scale().domain([new Date(this.min), new Date(this.max)]).range([0, this.width]);
+        this.xAxis = d3.svg.axis().scale(scaleX).ticks(20);
+        this.svg = d3
+            .select("#" + containerId)
             .append("svg")
-            .attr("width", width)
+            .attr("width", this.width)
             .attr("height", 500);
 
-        svg.append("g")
-            .attr("class", "x-axis")
-            .attr("transform", "translate(0," + 450 + ")")
-            .call(xAxis);
+
+        this.reset = function() {
+            this.container.innerHTML = '';
+        };
+
+        this.drawChart = function() {
+            this.svg.append("g")
+                .attr("class", "x-axis")
+                .attr("transform", "translate(0," + 450 + ")")
+                .call(xAxis);
+
+            var tip = d3.tip()
+                .attr('class', 'd3-tip')
+                .offset([-10, 0])
+                .html(function(d) {
+                    return "Took " + moment.duration(new Date(d.end) - new Date(d.start)).humanize();
+                });
+
+            this.svg.call(tip);
+
+            var rect = this.svg.selectAll("rect")
+                .data(this.data)
+                .enter()
+                .append("rect");
 
 
-        var tip = d3.tip()
-            .attr('class', 'd3-tip')
-            .offset([-10, 0])
-            .html(function(d) {
-                return "Took " + moment.duration(new Date(d.end) - new Date(d.start)).humanize();
-            });
-
-        svg.call(tip);
-
-        var rect = svg.selectAll("rect")
-            .data(data)
-            .enter()
-            .append("rect");
-
-
-        rect.attr("x", function(d) {
-            return scaleX(new Date(d.start));
-        })
-            .attr("y", function(d, i) {
-                return (i + 1) * 40;
+            rect.attr("x", function(d) {
+                return this.scaleX(new Date(d.start));
             })
-            .attr("width", function(d) {
-                return scaleX(new Date(d.end)) - scaleX(new Date(d.start));
+                .attr("y", function(d, i) {
+                    return (i + 1) * 40;
+                })
+                .attr("width", function(d) {
+                    return this.scaleX(new Date(d.end)) - this.scaleX(new Date(d.start));
+                })
+                .attr("height", 20)
+                .attr("fill", function(d, i) {
+                    return this.colours[i];
+                })
+                .on('mouseover', tip.show)
+                .on('mouseout', tip.hide);
+
+            var text = this.svg.selectAll("text.label")
+                .data(this.data)
+                .enter()
+                .append("text");
+
+            text.attr("x", function(d) {
+                return this.scaleX(new Date(d.start));
             })
-            .attr("height", 20)
-            .attr("fill", function(d, i) {
-                return colours[i];
-            })
-            .on('mouseover', tip.show)
-            .on('mouseout', tip.hide);
+                .attr("y", function(d, i) {
+                    return (i + 1) * 40 - 2;
+                })
+                .attr("class", "label")
+                .attr("text-anchor", "start")
+                .attr("fill", "black")
+                .text(function(d) {
+                    return d.name;
+                });
 
-        var text = svg.selectAll("text.label")
-            .data(data)
-            .enter()
-            .append("text");
+            function make_x_axis() {
+                return d3.svg.axis()
+                    .scale(this.scaleX)
+                    .orient("bottom")
+                    .ticks(20)
+            }
 
-        text.attr("x", function(d) {
-            return scaleX(new Date(d.start));
-        })
-            .attr("y", function(d, i) {
-                return (i + 1) * 40 - 2;
-            })
-            .attr("class", "label")
-            .attr("text-anchor", "start")
-            .attr("fill", "black")
-            .text(function(d) {
-                return d.name;
-            });
-
-        function make_x_axis() {
-            return d3.svg.axis()
-                .scale(scaleX)
-                .orient("bottom")
-                .ticks(20)
-        }
-
-        svg.append("g")
-            .attr("class", "grid")
-            .attr("transform", "translate(0,450)")
-            .call(make_x_axis()
-                .tickSize(-450, 0, 0)
-                .tickFormat(""));
-    };
+            this.svg.append("g")
+                .attr("class", "grid")
+                .attr("transform", "translate(0,450)")
+                .call(make_x_axis()
+                    .tickSize(-450, 0, 0)
+                    .tickFormat(""));
+        };
+    }
 }
